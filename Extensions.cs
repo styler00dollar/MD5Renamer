28c278123d8977f9540760ec80c9e547b93ffcbe
sagerobert <sagerobert@googlemail.com>
2017-12-21 15:43:20 +0100

    initial checkin
    initial checkin;

---
+++ Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Md5RenamerConsole
+{
+	public static class Extensions
+	{
+		public static List<string> Clean( this List<string> list )
+		{
+			string testAgainst = Path.GetFileNameWithoutExtension( Program.exe ).ToUpper();
+			string testee = string.Empty;
+
+			for ( int i = list.Count - 1; i >= 0; i-- )
+			{
+				testee = Path.GetFileName( list[ i ] ).ToUpper();
+				if ( testee.StartsWith( testAgainst ) ||
+					 testee.Equals( "DESKTOP.INI" ) )
+				{
+					list.Remove( list[ i ] );
+				}
+			}
+
+			return list;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Md5Renamer    
+<br />
+
+Simple windows console application which scans all files in the directory it is started from and renames them to their own MD5 sum.
+If such a filename already exists, the renaming will be skipped.
+
+<br />
+
+```
+Md5RenamerConsole.exe [.] [(--dry-run|-dr)]
+
+Version 1.0
+- without any arguments, this help is displayed.
+- the dot is mandatory to prevent accidentally running of the program.
+- '--dry-run' or '-d' just writes logs, but doesn't touch a file (case-insensitive).
+```
+<br />
+
+*sagerobert*
